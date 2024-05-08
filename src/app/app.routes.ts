371bb7e72854846af89b30b6e4cf36d9959643d4
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-08 23:14:03 +0200

    Navbar created and router will navigate to pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UserListComponent } from './user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    },
+    {
+        path: 'userlist',
+        component: UserListComponent
+    }
+];
