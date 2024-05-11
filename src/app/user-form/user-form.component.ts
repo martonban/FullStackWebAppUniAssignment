80402f00a9fdf8949630d376b4b363b9566a94eb
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-11 23:57:21 +0200

    Add User DONE

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { UserDTO } from '../../../models';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
+  formBuilder = inject(FormBuilder);
+  userService = inject(UserService);
+  // Angular ROUTER KELL!!
+  router = inject(Router);
 
+  userForm = this.formBuilder.group<UserDTO>({
+    id: 0,
+    firstName: '',
+    lastName: '',
+    birthDay: new Date(),
+    tajNumber: 0,
+  });
+
+  saveUser() {
+    const user = this.userForm.value as UserDTO;
+
+    this.userService.create(user).subscribe({
+      next: () => {
+        // TODO: notification
+        this.router.navigateByUrl('/userlist');
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
