3a56e43b9871210f90dee68ea7ffe46b2406452d
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-11 22:23:41 +0200

    Add user init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'userlist',
         component: UserListComponent
+    },
+    {
+        path: 'add-user',
+        component: UserFormComponent
     }
 ];
