18afe1541d4018c764811b82e4ba89a388fef029
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-12 21:45:01 +0200

    visit form init

---
+++ src/app/visit-form/visit-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './visit-form.component.html',
+  styleUrl: './visit-form.component.css'
+})
+export class VisitFormComponent {
+
+}
