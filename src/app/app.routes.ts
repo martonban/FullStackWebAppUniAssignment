a6d12c1439be65788525fe06eb9ee57d311dbfe1
Márton Bán <30813844+martonban@users.noreply.github.com>
2024-05-14 01:21:29 +0200

    typo fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserFormComponent } from './user-form/user-form.component';
-import { VisitStartComponent } from './visit-start/visit-start.component';
 import { VisitFormComponent } from './visit-form/visit-form.component';
 
 export const routes: Routes = [
